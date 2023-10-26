182545e5706be69cfaff040a0359256c223db495
Aste21 <131811810+Aste21@users.noreply.github.com>
2023-10-26 09:33:22 +0200

    Update TestStack.cpp

--- TestStack.cpp
+++ TestStack.cpp
@@ -8,15 +8,22 @@ void testAssignmentOperatorWithDifferentStackSizes(const Stack& s1);
 void testSelfAssignment(const Stack& s1);
 void testAssignmentOperatorWithEqualStackSizes(const Stack& s1);
 void testPop(Stack& s1);
+void testFuctionByValue(const Stack s1);
+void testFunctionByReference(const Stack& s1);
+void testCopyConstructor(const Stack s1);
+
 
 int main(int argc, char* argv[]) {
     Stack s1;
     testPush(s1);
+    testCopyConstructor(s1);
     testAssignmentOperatorWithEmptyStack(s1);
     testAssignmentOperatorWithDifferentStackSizes(s1);
     testSelfAssignment(s1);
     testAssignmentOperatorWithEqualStackSizes(s1);
     testPop(s1);
+    testFuctionByValue(s1);
+    testFunctionByReference(s1);
 
     printf("\n");
     return 0;
@@ -33,6 +40,17 @@ void testPush(Stack& s1) {
     s1.push(3);
     printf("\ns1.push(3), state of s1: \n");
     s1.printStack();
+}
+
+void testCopyConstructor(const Stack s1)
+{
+    printf("\n\nTest of copy constructor: \n\n");
+    Stack s2(s1);
+    printf("s2(s1):\n");
+    printf("State of s1: ");
+    s1.printStack();
+    printf("\nState of s2: ");
+    s2.printStack();
 }
 
 void testAssignmentOperatorWithEmptyStack(const Stack& s1) {
@@ -81,3 +99,16 @@ void testPop(Stack& s1) {
     printf("Popped element from s1: %d, state of s1 after pop: \n", s1.pop());
     s1.printStack();
 }
+
+void testFuctionByValue(const Stack s1)
+{
+    printf("\n\n");
+    s1.printStack();
+}
+
+void testFunctionByReference(const Stack &s1)
+{
+    printf("\n\n");
+    s1.printStack();
+}
+
